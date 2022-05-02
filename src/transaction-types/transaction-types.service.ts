06608a39b0c71647562d5e69100efe4b98b84b59
Ayorinde Akindemowo <ayorindeakindemowo@Ayorindes-MacBook-Pro.local>
2022-05-02 23:13:55 +0100

    transaction-types

--- src/transaction-types/transaction-types.service.ts
+++ src/transaction-types/transaction-types.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { CreateTransactionTypesDto } from './dto/create-transaction-type.dto';
+import Transactiontypes from './entities/transaction-types-entity';
 
 @Injectable()
-export class TransactionTypesService {}
+export class TransactionTypesService {
+  async create(
+    createTransactionTypesDto: CreateTransactionTypesDto,
+  ): Promise<Transactiontypes> {
+    const transactionType = Transactiontypes.create(createTransactionTypesDto);
+    return await transactionType.save();
+  }
+
+  async show(): Promise<Transactiontypes[]> {
+    return await Transactiontypes.find();
+  }
+
+  async showById(id: number): Promise<Transactiontypes> {
+    return await Transactiontypes.findOneBy({ id });
+  }
+}
