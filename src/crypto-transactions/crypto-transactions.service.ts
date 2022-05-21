d9e9cb457ef9b6711f2d4b3dc53726432ed11c9f
Ayorinde Akindemowo <ayorindeakindemowo@Ayorindes-MacBook-Pro.local>
2022-05-21 22:32:44 +0100

    crypto-service-module

--- src/crypto-transactions/crypto-transactions.service.ts
+++ src/crypto-transactions/crypto-transactions.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CryptoTransactionsService {}
+export class CryptoTransactionsService {
+  //todo
+  async findAll(): Promise<any> {
+    return 'This action returns all transactions';
+  }
+
+  async findOne(id: string): Promise<any> {
+    return `This action returns a #${id} transaction`;
+  }
+
+  async create(transaction: any): Promise<any> {
+    return `This action adds a new transaction: ${transaction}`;
+  }
+}
