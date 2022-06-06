36842c70bbf3a8927dc246c0b5c108f44f40a340
Ayorinde Akindemowo <ayorindeakindemowo@Ayorindes-MacBook-Pro.local>
2022-06-06 23:07:40 +0100

    get all transaction

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 
 @Controller('transactions')
 export class TransactionsController {
   constructor(private readonly transactionsService: TransactionsService) {}
+
+  @Get()
+  show() {
+    return this.transactionsService.show();
+  }
 }
